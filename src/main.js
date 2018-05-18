cd42ea0d87fa691e7266dc9a1f72669db530c0ff
Zephyr Wong <zephyrwong@oursky.com>
2018-05-18 14:10:41 +0800

    Add Skygear SDK

--- src/main.js
+++ src/main.js
@@ -4,10 +4,20 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import skygear from 'skygear'
 import App from './App'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+skygear.config({
+  'endPoint': 'https://reactdinnerpoll.skygeario.com/',
+  'apiKey': '8b05aba14a3e4b53a93277ae93a53c13'
+}).then(() => {
+  console.log('skygear container is now ready for making API calls.')
+}, (error) => {
+  console.error(error)
+})
 
 /* eslint-disable no-new */
 new Vue({
